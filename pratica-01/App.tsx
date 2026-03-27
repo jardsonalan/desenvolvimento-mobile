047dfbd714de72a72316dc7862d86d8d12df960b
Jardson Alan <jardsonalan132@gmail.com>
2026-03-26 22:50:27 -0300

    feat: Adiciona os componentes e estilos no App.tsx

--- pratica-01/App.tsx
+++ pratica-01/App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { BoasVindas } from '@/components/BoasVindas';
+import { CardProximaAula } from '@/components/CardProximaAula';
+import { CardPendencia } from '@/components/CardPendencia';
+import { Rodape } from '@/components/Rodape';
+
+import { View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BoasVindas nameStudent='Jardson' course='Sistemas para Internet' />
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.title}>Próximas aulas:</Text>
+          <CardProximaAula classroom='Desenvolvimento Mobile' description='Quinta-feira e Sexta-feira' />
+        </View>
+        <View>
+          <Text style={styles.title}>Pendências:</Text>
+          <CardPendencia pending='Exercício de Revisão - Encontro 1' />
+          <CardPendencia pending='Exercício de Estudo - Encontro 1' />
+        </View>
+      </View>
+      <Rodape semester='5° Período' />
     </View>
   );
 }
@@ -13,8 +29,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 34,
+    backgroundColor: '#f4f4f4',
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    paddingTop: 14,
+    paddingLeft: 14,
+    fontSize: 16,
+    fontWeight: 700,
+    color: '#1c1c1c'
   },
 });
